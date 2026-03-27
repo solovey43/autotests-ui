b7424dd24ba62e404a9f39933337eac92c798be3
solovey43 <tsypinaea@gmail.com>
2026-03-27 09:47:25 +0300

    Знакомство с PageComponent

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,11 +1,15 @@
 from playwright.sync_api import Page, expect
 
+from components.navigation.navbar_component import NavbarComponent
 from pages.base_page import BasePage
 
 
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        # Добавляем компонент Navbar
+        self.navbar = NavbarComponent(page)
 
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
@@ -21,7 +25,6 @@ class DashboardPage(BasePage):
         self.scores_title = page.get_by_test_id('scores-widget-title-text')
         self.scores_chart = page.get_by_test_id('scores-scatter-chart')
 
-
     def check_visible_dashboard_title(self):
         expect(self.dashboard_title).to_be_visible()
         expect(self.dashboard_title).to_have_text('Dashboard')
@@ -31,19 +34,17 @@ class DashboardPage(BasePage):
         expect(self.students_title).to_have_text('Students')
         expect(self.students_chart).to_be_visible()
 
-    def check_visible_activities_chart(self):
-        expect(self.activities_title).to_be_visible()
-        expect(self.activities_title).to_have_text('Activities')
-        expect(self.activities_chart).to_be_visible()
-
     def check_visible_courses_chart(self):
         expect(self.courses_title).to_be_visible()
         expect(self.courses_title).to_have_text('Courses')
         expect(self.courses_chart).to_be_visible()
 
+    def check_visible_activities_chart(self):
+        expect(self.activities_title).to_be_visible()
+        expect(self.activities_title).to_have_text('Activities')
+        expect(self.activities_chart).to_be_visible()
+
     def check_visible_scores_chart(self):
         expect(self.scores_title).to_be_visible()
         expect(self.scores_title).to_have_text('Scores')
         expect(self.scores_chart).to_be_visible()
-
-
